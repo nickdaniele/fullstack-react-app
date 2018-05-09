dcf48096ff0e0e730cffeb0d5a06a28a37f094e4
Nick Daniele <ngdaniele@#gmail.com>
2018-05-08 23:19:32 -0700

    Add eslint and prettier to project. Created npm scripts

---
+++ js/ClientApp.jsx
@@ -0,0 +1,21 @@
+const ce = React.createElement;
+
+const MyTitle = function(props) {
+  return ce(
+    'div',
+    null,
+    ce('h1', { style: { color: props.color } }, props.title)
+  );
+};
+
+const MyFirstComponent = function() {
+  return ce(
+    'div',
+    null,
+    ce(MyTitle, { color: 'rebeccapurple', title: 'House of Cards' }),
+    ce(MyTitle, { color: 'peru', title: 'Orange is the New Black' }),
+    ce(MyTitle, { color: 'burlywood', title: 'Stranger Things' })
+  );
+};
+
+ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
